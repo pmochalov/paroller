294b48e2edc8b68f4612d6b2b6a89162312ad478
pmochalov <52402489+pmochalov@users.noreply.github.com>
2024-12-06 11:59:39 +0300

    исправлен баг в классе кнопки  в компоненте Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,10 +10,15 @@ type ButtonProps = {
 
 const Button: React.FC<ButtonProps> = ({ isChecked, children, ...props }) => {
     const setBtnClass = (isChecked: boolean) => {
+        if (isChecked === undefined) {
+            return `${s.button}`;
+        }
+
         return isChecked ? `${s.button} ${s.button_checked}` : `${s.button}`;
     };
 
-    const btnClass = isChecked !== undefined ? setBtnClass(isChecked) : "";
+    const btnClass =
+        isChecked !== undefined ? setBtnClass(isChecked) : `${s.button}`;
 
     return (
         <button type='button' className={btnClass} {...props}>
